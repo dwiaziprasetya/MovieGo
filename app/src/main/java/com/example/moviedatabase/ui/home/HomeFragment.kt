65c410127806d3f02e4c33b55768356509a8a022
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2023-11-28 21:09:57 +0700

    Menyelesaikan image slider dan memperbarui beberapa file

--- app/src/main/java/com/example/moviedatabase/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/moviedatabase/ui/home/HomeFragment.kt
@@ -1,19 +1,30 @@
 package com.example.moviedatabase.ui.home
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.moviedatabase.R
+import com.example.moviedatabase.adapter.ImageSliderAdapter
 import com.example.moviedatabase.databinding.FragmentHomeBinding
+import com.example.moviedatabase.model.ImageData
 
 class HomeFragment : Fragment() {
 
-private var _binding: FragmentHomeBinding? = null
-  // This property is only valid between onCreateView and
-  // onDestroyView.
-  private val binding get() = _binding!!
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var adapter : ImageSliderAdapter
+    private val list = ArrayList<ImageData>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        list.addAll(getListImageSlider())
+        adapter = ImageSliderAdapter(list)
+        binding.viewPager.adapter = adapter
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,7 +38,18 @@ private var _binding: FragmentHomeBinding? = null
         return binding.root
     }
 
-override fun onDestroyView() {
+    @SuppressLint("Recycle")
+    private fun getListImageSlider() : ArrayList<ImageData> {
+        val dataImage = resources.obtainTypedArray(R.array.image_slider)
+        val listImage = ArrayList<ImageData>()
+        for (i in 0 until dataImage.length()) {
+            val image = ImageData(dataImage.getResourceId(i,0))
+            listImage.add(image)
+        }
+        return listImage
+    }
+
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
