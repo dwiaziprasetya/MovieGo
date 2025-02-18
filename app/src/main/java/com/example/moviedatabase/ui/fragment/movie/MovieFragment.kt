42dde03e22da7e3ae0436b203eba7d3f5319c3b6
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-02-19 00:33:19 +0700

    Add progress bar in fragment movie recycyle view

--- app/src/main/java/com/example/moviedatabase/ui/fragment/movie/MovieFragment.kt
+++ app/src/main/java/com/example/moviedatabase/ui/fragment/movie/MovieFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.moviedatabase.data.LoadingStateAdapter
 import com.example.moviedatabase.databinding.FragmentMovieBinding
@@ -58,9 +60,16 @@ class MovieFragment : Fragment() {
             footer = LoadingStateAdapter()
         )
 
-        viewModel.movie.observe(viewLifecycleOwner, {
+        viewModel.movie.observe(viewLifecycleOwner) {
             adapter.submitData(lifecycle, it)
-        })
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            val isLoading = loadState.refresh is LoadState.Loading
+            binding.progressBar.isVisible = isLoading
+            binding.rvDiscoverMovies.isVisible = !isLoading
+        }
     }
+
 
 }
