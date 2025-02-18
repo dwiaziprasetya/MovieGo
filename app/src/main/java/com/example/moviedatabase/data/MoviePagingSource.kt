850491b9d16ea87563ef1692b7a2449658fc8867
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-02-18 20:48:57 +0700

    Add MoviePagingSource.kt file

---
+++ app/src/main/java/com/example/moviedatabase/data/MoviePagingSource.kt
@@ -0,0 +1,35 @@
+package com.example.moviedatabase.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.moviedatabase.data.remote.response.DiscoverMovieItem
+import com.example.moviedatabase.data.remote.retrofit.ApiService
+
+class MoviePagingSource(
+    private val apiService: ApiService
+) : PagingSource<Int, DiscoverMovieItem>() {
+
+    private companion object {
+        const val INITIAL_PAGE = 1
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, DiscoverMovieItem>): Int? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, DiscoverMovieItem> {
+        return try {
+            val position = params.key ?: INITIAL_PAGE
+            val responseData = apiService.getDiscoverMovie(position)
+
+            LoadResult.Page(
+                data = responseData.results,
+                prevKey = if (position == INITIAL_PAGE) null else position - 1,
+                nextKey = if (responseData.results.isEmpty()) null else position + 1
+            )
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+    }
+
+}
