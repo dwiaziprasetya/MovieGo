a11dff021e539bb46b0d8dd350b24c2dc795a981
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-09 22:53:30 +0700

    Add and modify NowPlayingViewModel

---
+++ app/src/main/java/com/example/moviedatabase/presentation/screen/nowplaying/NowPlayingViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.moviedatabase.presentation.screen.nowplaying
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.moviedatabase.data.remote.response.UpComingMovieItem
+import com.example.moviedatabase.data.repository.MovieDatabaseRepository
+
+class NowPlayingViewModel(
+    private val repository: MovieDatabaseRepository
+): ViewModel() {
+    val movie: LiveData<PagingData<UpComingMovieItem>> =
+        repository.getUpComingMovieDataPaging().cachedIn(viewModelScope)
+}
