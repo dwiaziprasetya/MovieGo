1f3e3c84d3e5aa898c6bbd6c3a282fa551f058c1
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-10 08:46:26 +0700

    Modify PopularActivity.kt file

--- app/src/main/java/com/example/moviedatabase/presentation/screen/popular/PopularActivity.kt
+++ app/src/main/java/com/example/moviedatabase/presentation/screen/popular/PopularActivity.kt
@@ -4,14 +4,22 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.moviedatabase.databinding.ActivityPopularBinding
+import com.example.moviedatabase.presentation.adapter.PagingRvPopularMovieAdapter
+import com.example.moviedatabase.presentation.adapter.ShimmerItemMovieAdapter
+import com.example.moviedatabase.utils.ViewModelFactory
 
 class PopularActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPopularBinding
+    private lateinit var factory: ViewModelFactory
+    private val viewModel by viewModels<PopularViewModel> { factory }
+    private val adapter = PagingRvPopularMovieAdapter()
+    private lateinit var shimmerAdapter : ShimmerItemMovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
