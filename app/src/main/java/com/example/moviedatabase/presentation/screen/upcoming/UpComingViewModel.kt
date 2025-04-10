e087de8322443f7a70d8121a4c54229080d09d6c
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-10 08:32:17 +0700

    Modify UpComingViewModel.kt file

--- app/src/main/java/com/example/moviedatabase/presentation/screen/upcoming/UpComingViewModel.kt
+++ app/src/main/java/com/example/moviedatabase/presentation/screen/upcoming/UpComingViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.moviedatabase.presentation.screen.upcoming
 
-class UpComingViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.moviedatabase.data.remote.response.UpComingMovieItem
+import com.example.moviedatabase.data.repository.MovieDatabaseRepository
+
+class UpComingViewModel(
+    private val repository: MovieDatabaseRepository
+): ViewModel() {
+    val movie: LiveData<PagingData<UpComingMovieItem>> =
+        repository.getUpComingMovieDataPaging().cachedIn(viewModelScope)
 }
