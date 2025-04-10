805162b13967eb558010711f229580c4d1102524
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-10 08:31:05 +0700

    Modify PopularViewModel.kt file

--- app/src/main/java/com/example/moviedatabase/presentation/screen/popular/PopularViewModel.kt
+++ app/src/main/java/com/example/moviedatabase/presentation/screen/popular/PopularViewModel.kt
@@ -1,4 +1,16 @@
 package com.example.moviedatabase.presentation.screen.popular
 
-class PopularViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.moviedatabase.data.remote.response.PopularMovieItem
+import com.example.moviedatabase.data.repository.MovieDatabaseRepository
+
+class PopularViewModel(
+    private val repository: MovieDatabaseRepository
+): ViewModel() {
+    val movie: LiveData<PagingData<PopularMovieItem>> =
+        repository.getPopularMovieDataPaging().cachedIn(viewModelScope)
 }
