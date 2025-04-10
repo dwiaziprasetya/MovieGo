0d12d5d872888d8595522530db0baf75a3a035d3
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-10 08:23:44 +0700

    Modify PopularMoviePagingSource.kt file

--- app/src/main/java/com/example/moviedatabase/data/remote/paging/PopularMoviePagingSource.kt
+++ app/src/main/java/com/example/moviedatabase/data/remote/paging/PopularMoviePagingSource.kt
@@ -1,4 +1,36 @@
 package com.example.moviedatabase.data.remote.paging
 
-class PopularMoviePagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.moviedatabase.data.remote.response.PopularMovieItem
+import com.example.moviedatabase.data.remote.retrofit.ApiService
+
+class PopularMoviePagingSource(
+    private val apiService: ApiService
+): PagingSource<Int, PopularMovieItem>() {
+    private companion object {
+        const val INTIAL_PAGE = 1
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, PopularMovieItem>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularMovieItem> {
+        return try {
+            val position = params.key ?: INTIAL_PAGE
+            val responseData = apiService.getPopularMovies(position)
+
+            LoadResult.Page(
+                data = responseData.results,
+                prevKey = if (position == INTIAL_PAGE) null else position - 1,
+                nextKey = if (responseData.results.isEmpty()) null else position + 1
+            )
+        } catch (e: Exception) {
+            return LoadResult.Error(e)
+        }
+     }
 }
