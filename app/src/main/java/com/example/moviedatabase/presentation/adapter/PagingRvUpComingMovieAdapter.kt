82d3d695a74297be57232821246c19cc7545b9d8
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2025-04-10 09:05:36 +0700

    Modify PagingRvUpComingMovieAdapter.kt file

--- app/src/main/java/com/example/moviedatabase/presentation/adapter/PagingRvUpComingMovieAdapter.kt
+++ app/src/main/java/com/example/moviedatabase/presentation/adapter/PagingRvUpComingMovieAdapter.kt
@@ -1,4 +1,66 @@
 package com.example.moviedatabase.presentation.adapter
 
-class PagingRvUpComingMovieAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.moviedatabase.BuildConfig
+import com.example.moviedatabase.R
+import com.example.moviedatabase.data.remote.response.UpComingMovieItem
+import com.example.moviedatabase.databinding.ItemMovies2Binding
+
+class PagingRvUpComingMovieAdapter: PagingDataAdapter<UpComingMovieItem, PagingRvUpComingMovieAdapter.ViewHolder>(
+    DIFF_CALLBACK) {
+    private lateinit var onItemCallback : OnitemClickCallback
+
+    class ViewHolder(val binding : ItemMovies2Binding) : RecyclerView.ViewHolder(binding.root) {
+        private val imageBaseUrl = BuildConfig.BASE_IMAGE_URL_MOVIE_DB_W500
+        fun bind(movie : UpComingMovieItem){
+            Glide.with(itemView)
+                .load(imageBaseUrl + movie.posterPath)
+                .into(binding.imgItemPhoto)
+        }
+    }
+
+    companion object {
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<UpComingMovieItem>() {
+            override fun areItemsTheSame(
+                oldItem: UpComingMovieItem,
+                newItem: UpComingMovieItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: UpComingMovieItem,
+                newItem: UpComingMovieItem
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = getItem(position)
+        holder.bind(movie!!)
+        holder.binding.root.setOnClickListener {
+            onItemCallback.onItemClicked(movie.id)
+        }
+        holder.binding.itemMovies.startAnimation(android.view.animation.AnimationUtils.loadAnimation(holder.binding.itemMovies.context, R.anim.scale_up))
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMovies2Binding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnitemClickCallback){
+        this.onItemCallback = onItemClickCallback
+    }
+
+    interface OnitemClickCallback {
+        fun onItemClicked(movieId: Int)
+    }
 }
